023760666ccd8fa7aa60d4281c4b7cc1631c51c4
iamEzi77 <Talhyounas28@gmail.com>
2025-09-11 16:50:19 +0500

    Updated Project controller

--- myapp/app/controllers/projects_controller.rb
+++ myapp/app/controllers/projects_controller.rb
@@ -1,13 +1,19 @@
 class ProjectsController < ApplicationController
     before_action :authenticate_user!
     before_action :require_admin_or_employee
-
+    before_action :set_employee, only: [:index, :new, :create, :edit]
    def index
-      # @projects = Project.filter(params).merge(Project.search(params))
+    if params[:search].present?
+      @projects = Project.search(params[:search])
+    elsif params[:user_id].present? || params[:status].present? || params[:priority].present? ||
+          params[:due_date].present? || params[:category].present? || params[:created_from].present? ||
+          params[:created_to].present?
+      @projects = Project.filter(params)
+    else
       @projects = Project.all
-      @employees = User.where(role: 0)
+    end
+     @projects = apply_sort(@projects, params[:sort_by], params[:direction])
    end
-
 
     def show
       @project = Project.find(params[:id])
@@ -17,18 +23,15 @@ class ProjectsController < ApplicationController
       @project = Project.new
       @employees = User.where(role: 0)
     end
-
     def create
       @project = Project.new(project_params)
-      
       if @project.save
         if current_user.admin?
-          redirect_to projects_path, notice: "Project created successfully."
+          redirect_to admin_dashboard_index_path, notice: "Project created successfully."
         else
-          redirect_to projects_path, notice: "Project created successfully."
+          redirect_to employee_dashboard_index_path, notice: "Project created successfully."
         end
       else
-        @employees = User.where(role: 0) 
         render :new
       end
     end
@@ -36,7 +39,6 @@ class ProjectsController < ApplicationController
 
     def edit
       @project = Project.find(params[:id])
-      @employees = User.where(role: 0)
     end
 
     def update
@@ -44,9 +46,9 @@ class ProjectsController < ApplicationController
       
       if @project.update(project_params)
         if current_user.admin?
-          redirect_to projects_path, notice: "Project updated successfully."
+          redirect_to admin_dashboard_index_path, notice: "Project updated successfully."
         else
-          redirect_to projects_path, notice: "Project updated successfully."
+          redirect_to employee_dashboard_index_path, notice: "Project updated successfully."
         end
       else
         render :edit
@@ -61,9 +63,10 @@ class ProjectsController < ApplicationController
       if current_user.admin?
         redirect_to projects_path, notice: "Project deleted successfully."
       else
-        redirect_to projects_path, notice: "Project deleted successfully."
+        redirect_to employee_dashboard_index_path, notice: "Project deleted successfully."
       end
     end
+
 
 
     private
@@ -71,4 +74,27 @@ class ProjectsController < ApplicationController
     def project_params
       params.require(:project).permit(:title, :description, :due_date, :priority_level, :category, :status, :user_id)
     end
+
+    def set_employee
+      @employees = User.where(role: 0)
+    end
+
+    def apply_sort(projects, sort_by, direction)
+  return projects unless sort_by.present?
+
+  direction = %w[asc desc].include?(direction) ? direction : "asc"
+
+  case sort_by
+  when "due_date"
+    projects.order(due_date: direction)
+  when "priority"
+    projects.order(priority_level: direction)
+  when "created_at"
+    projects.order(created_at: direction)
+  when "title"
+    projects.order(title: direction)
+  else
+    projects
+  end
+end
 end
